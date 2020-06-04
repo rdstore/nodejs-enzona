1cfc063ff67d45e3b09719a63a1236d6ff2ba503
dannyweb86 <65356727+dannyweb86@users.noreply.github.com>
2020-06-04 16:53:25 -0700

    update
    Payments

--- src/payments.js
+++ src/payments.js
@@ -2,10 +2,10 @@
 
 var qs = require('qs');
 
-
 module.exports = function(config){
 
     const ezrequest = require('./request')(config)
+    var productsModel = require('./models/products')(config)
     
     const Payments = {
 
@@ -25,64 +25,9 @@ module.exports = function(config){
          * 
          */
         create: async (products) => {
-
-            return new Promise((resolve, reject) => {
-    
-                let total = 0
-                let totaltax = 0
-    
-                products.items.forEach(element => {
-                    total += element.price * element.quantity
-                    totaltax += element.tax * element.quantity
-                    element.price = element.price.toFixed(2)
-                    element.tax = element.tax.toFixed(2)
-                })
-    
-                total += totaltax + parseFloat(products.shipping)
-                let sendata = {}
-    
-                sendata.description = 'pagoami'
-                sendata.currency = 'CUP'
-    
-                sendata.amount = {
-                    total: total.toFixed(2)
-                }
-    
-                sendata.amount.details = {
-                    shipping: products.shipping.toFixed(2),
-                    tax: totaltax.toFixed(2),
-                    discount: "0.01",
-                    tip: "0.01"
-                }
-    
-                sendata.items = products.items
-    
-                sendata.merchant_op_id = 111125478251,
-                sendata.invoice_number = 7854,
-                sendata.return_url = config.url.confirmURL
-                sendata.cancel_url = config.url.cancelURL,
-                sendata.terminal_id = 12121,
-                sendata.buyer_identity_code = ''
-    
-                resolve(sendata)
-    
-                // axios.post(config.url.apiURL + '/payment/v1.0.0/payments', sendata, {
-                //     timeout: config.general.requestTimeout,
-                //     headers: {
-                //         'Authorization': 'Bearer ' + token.access_token,
-                //         'User-Agent': config.general.serviceName
-                //     }
-                // }).then( res => {
-                //     resolve(res.data)
-                // })
-                // .catch( err => {
-                //     if (err.response === undefined){
-                //         reject({error: 'Destino inaccesible o problema de red'})
-                //     }else{
-                //         reject(err.response.data)
-                //     }
-                // })
-    
+            return new Promise( async (resolve, reject) => {
+                //resolve(productsModel.build(products))
+                await ezrequest.run('POST', '/payment/v1.0.0/payments', productsModel.build(products)).then( res => { resolve(res) }).catch( err => { reject(err) })    
             })
         },
     
